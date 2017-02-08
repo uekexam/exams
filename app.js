a396f029a72140205d7c8b40b0f2efb3617011c8
15934359175 <cp1305452642@163.com>
2017-02-08 15:08:34 +0800

    this is exams

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("view engine","html");
+app.engine('html', require('ejs').renderFile);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -32,13 +33,13 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//app.use(function(err, req, res, next) {
+//  // set locals, only providing error in development
+//  res.locals.message = err.message;
+//  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//  // render the error page
+//  res.status(err.status || 500);
+//  res.render('error');
+//});
 app.listen(8080);
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,63 @@
 var express = require('express');
 var router = express.Router();
-
+var mysql=require("./mysql");
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('admin/main');
+  res.render('admin/index');
 });
-router.get('/', function(req, res, next) {
-  res.render('admin/main');
+router.use('/editquestion', function(req, res, next) {
+  mysql.query(`select * from type`,function(error,rows,field){
+    res.render("admin/add",{data:rows});
+  })
 });
+router.use('/showquestion', function(req, res, next) {
+  res.render('admin/list');
+});
+
+
+
+
+//类型管理
+router.use('/editType',function(req,res,next){
+  mysql.query(`select * from type`,function(error,rows,field){
+    res.render('admin/column',{data:rows});
+  })
+});
+router.use('/addType',function(req,res,next){
+  var name=req.body.name;
+  var del=req.body.del;
+  mysql.query(`insert into  type (name,del) values ('${name}','${del}') `,function(error,rows,field){
+    if(error){
+      res.send("数据储存出现错误！！");
+    }else{
+      res.redirect('/admin/editType');
+    }
+  })
+});
+
+
+
+
+
+//阶段管理
+router.use('/editStage',function(req,res,next){
+  res.render('admin/list')
+});
+router.use('/addStage',function(req,res,next){
+  var name=req.body.name;
+  var del=req.body.del;
+  var sid=req.body.s_id;
+  mysql.query(`insert into  stage (name,del,s_id) values ('${name}','${del}','${sid}') `,function(error,rows,field){
+    if(error){
+      res.send("数据储存出现错误！！");
+    }else{
+      res.redirect('/admin/editType');
+    }
+  })
+});
+
+
+
+
 
 module.exports = router;
